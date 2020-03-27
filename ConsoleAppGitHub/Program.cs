9f3cc7c3c4c1c6a8507c852b57f90dfde13ec468
MiyuruCG <miyurugunatilake@gmail.com>
2020-03-28 00:34:30 +0530

    Add project files.

---
+++ ConsoleAppGitHub/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleAppGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Adding VS code to GitHub.....");
+        }
+    }
+}
